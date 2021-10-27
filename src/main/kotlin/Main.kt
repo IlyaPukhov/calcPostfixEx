c904e5b67b43b821c1717073c4815f0db5fdf346
Ilya Pukhov <IlyaPukhov@github.com>
2021-10-28 01:55:39 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+import kotlin.math.sin
+
+fun main() {
+    print("Введите постфиксное выражение для вычисления: ")
+    val s: String = readLine().toString()
+    println("Результат: ${calc(s)}")
+}
+
+fun calc(s: String): Double {
+    // Преобразуем исходную строку в подходящий нам массив операндов и операций
+    val exp = s.replace("sin", " sin").split(" ").map{f -> f.trim()}
+    val st = mutableListOf<Double>() // Создаём стек для чисел
+    for (i in exp.indices) {
+        try {
+            when (exp[i]) { // Проходимся по всем элементам массива
+                "+" -> {  // Если встречается "+", то вынимаем 2 последних числа из стека и добавляем в конец их сумму
+                    val r = st.last()
+                    st.removeLast()
+                    val l = st.last()
+                    st.removeLast()
+                    st.add(l + r)
+                }
+                "-" -> { // Если встречается "-", то вынимаем 2 последних числа из стека и добавляем в него их разность
+                    val r = st.last()
+                    st.removeLast()
+                    val l = st.last()
+                    st.removeLast()
+                    st.add(l - r)
+                }
+                "*" -> {  // Если встречается "*", то вынимаем 2 последних числа из стека и добавляем в него их произведение
+                    val r = st.last()
+                    st.removeLast()
+                    val l = st.last()
+                    st.removeLast()
+                    st.add(l * r)
+                }
+                "/" -> {  // Если встречается "/", то вынимаем 2 последних числа из стека и добавляем в конец их частное
+                    val r = st.last()
+                    st.removeLast()
+                    val l = st.last()
+                    st.removeLast()
+                    st.add(l / r)
+                }
+                "sin" -> { // Если встречается "sin", то вынимаем последнее число из стека и добавляем в конец sin от него
+                    val x = st.last()
+                    st.removeLast()
+                    st.add(sin(x))
+                }
+                else -> st.add(exp[i].toDouble()) // В остальных случаях добавляем число в стек
+            }
+        } catch (e: NoSuchElementException) { // Если количество чисел в стеке недостаточно для операции, выдаём ошибку
+            throw Error("Неверное выражение!")
+        }
+    }
+    if (st.size != 1) { // Если после всех операций в стеке осталось более 1 числа, выдаём ошибку
+        throw Error("Неверное выражение!")
+    }
+    return st[0] // Если в стеке осталось 1 число, то возвращаем его (это и есть результат выражения)
+}
